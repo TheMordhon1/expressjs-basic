dd864d3128e16e1b6ee06c2de6e1c1c763a93660
the mordhon <romdhonm12@gmail.com>
2022-11-18 17:11:16 +0700

    basic routing with express js

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
 	res.send('Hello World!')
 })
 
+app.post('/post', (req, res) => {
+	res.send('Got a POST request')
+})
+
+app.put('/put', (req, res) => {
+	res.send('Got a PUT request at /user')
+})
+
+app.delete('/delete', (req, res) => {
+	res.send('Got a DELETE request at /user')
+})
+
 app.listen(port, () => {
 	console.log(`Example app listening on port http://localhost:${port}`)
 })
